9c3fc1ef9f26bebf6943987babaa909c4ab028e4
stas.bozhko <stas.bozhko@gmail.com>
2025-08-05 15:13:25 +0200

    Force update to latest version 2.7.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2ea115e809e8713b2c9bbe196a1c512d",
+const RESOURCES = {"flutter_bootstrap.js": "718f67d12bba378c03af387173959ede",
 "version.json": "58e247f6f5150bc3880b0ad34ecdb575",
 "index.html": "8be712dbfc64ed97e6d7901d9cd07a6a",
 "/": "8be712dbfc64ed97e6d7901d9cd07a6a",
-"main.dart.js": "6723364bf708e9237d4aac9d8c4400d6",
+"main.dart.js": "056e10e16ba2ff18f95faa399a3e3512",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
